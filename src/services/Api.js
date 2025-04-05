2eda49978f46c23fa39eaafc35f91f7db30c438d
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2025-04-05 15:37:21 +0530

    define headers in API services

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const baseURL = `https://jsonplaceholder.typicode.com`;
-const api = axios.create({ baseURL });
+const api = axios.create({ baseURL, headers : {"Content-Type" : "application/json"} });
 
 export const getPosts = (resource) => api.get(resource);
